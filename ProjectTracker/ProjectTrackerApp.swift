4e802677d1b6e4e86f28c484d90e1f4bcb851a91
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2025-06-24 13:03:48 -0400

    Initial Commit

---
+++ ProjectTracker/ProjectTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTrackerApp.swift
+//  ProjectTracker
+//
+//  Created by Dan June on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
